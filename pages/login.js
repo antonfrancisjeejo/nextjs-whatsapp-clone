a8afb99471af8e1672fed0ba7d0d7f5222131d12
antonfrancisjeejo <anton13jeejo@gmail.com>
2021-05-21 20:58:42 +0530

    Initial commit

---
+++ pages/login.js
@@ -0,0 +1,50 @@
+import { Button } from "@material-ui/core";
+import Head from "next/head";
+import styled from "styled-components";
+import { auth, provider } from "../firebase";
+
+function login() {
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch(alert);
+  };
+
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+
+      <LoginContainer>
+        <Logo src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/598px-WhatsApp.svg.png" />
+        <Button variant="outlined" onClick={signIn}>
+          Sign in with Google
+        </Button>
+      </LoginContainer>
+    </Container>
+  );
+}
+
+export default login;
+
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: whitesmoke;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 100px;
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
+`;
+
+const Logo = styled.img`
+  height: 200px;
+  width: 200px;
+  margin-bottom: 50px;
+`;
